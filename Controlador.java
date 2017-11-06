1628dbbe0d365162c4b160c07477bad44aa361b5
Santi <32668940+SantiPedros@users.noreply.github.com>
2017-11-06 11:07:34 +0100

    Add files via upload

---
+++ Controlador.java
@@ -0,0 +1,7 @@
+
+package supercoche;
+
+
+public class Controlador {
+    
+}
---
+++ SuperCoche.java
@@ -0,0 +1,15 @@
+
+package supercoche;
+
+
+public class SuperCoche {
+
+   
+    public static void main(String[] args) {
+        
+        
+        
+        
+    }
+    
+}
