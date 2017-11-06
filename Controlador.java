e83899077f390d1f8fa24ceded6c6da735ff0e5c
FeeeR12 <32671971+FeeeR12@users.noreply.github.com>
2017-11-06 11:20:48 +0100

    Update Controlador.java

--- Controlador.java
+++ Controlador.java
@@ -3,5 +3,32 @@ package supercoche;
 
 
 public class Controlador {
+    private Vista view;
+private Modelo model;
+    @Override
+    public void keyTyped(KeyEvent ke) {
+        
+    }
+
+    @Override
+    public void keyPressed(KeyEvent ke) {
+        if(ke.getKeyCode()==KeyEvent.VK_UP){
+            
+        }
+        if(ke.getKeyCode()==KeyEvent.VK_DOWN){
+            
+        }
+        if(ke.getKeyCode()==KeyEvent.VK_LEFT){
+            
+        }
+        if(ke.getKeyCode()==KeyEvent.VK_RIGHT){
+            
+        }
+    }
+
+    @Override
+    public void keyReleased(KeyEvent ke) {
+        
+    }
     
 }
