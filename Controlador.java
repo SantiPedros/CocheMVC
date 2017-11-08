d84004052cb1d638f6d27375ef41edbb210b5ba4
Santi <32668940+SantiPedros@users.noreply.github.com>
2017-11-08 17:12:30 +0100

    Update Controlador.java

--- Controlador.java
+++ Controlador.java
@@ -3,8 +3,13 @@ package supercoche;
 
 
 public class Controlador {
-    private Vista view;
-private Modelo model;
+     private Modelo model;
+private Vista view;
+public Controlador(){
+    model=new Modelo();
+    view=new Vista(model.getCoche(),model.getHer(),model.getGas());
+    view.addKeyListener(this);
+}
     @Override
     public void keyTyped(KeyEvent ke) {
         
@@ -12,23 +17,34 @@ private Modelo model;
 
     @Override
     public void keyPressed(KeyEvent ke) {
+        
+        if(ke.getKeyCode()==KeyEvent.VK_ESCAPE){
+            System.exit(0);
+        }
+        
         if(ke.getKeyCode()==KeyEvent.VK_UP){
-            
+            model.moverArriba();
         }
         if(ke.getKeyCode()==KeyEvent.VK_DOWN){
-            
+            model.moverAbajo();
         }
         if(ke.getKeyCode()==KeyEvent.VK_LEFT){
-            
+            model.moverIzquierda();
         }
         if(ke.getKeyCode()==KeyEvent.VK_RIGHT){
-            
+            model.moverDerecha();
         }
+        model.comprobar();
+        view.setCoche(model.getCoche());
+        view.setGas(model.getGas());
+        view.setHer(model.getHer());
+        view.repaint();
     }
 
     @Override
     public void keyReleased(KeyEvent ke) {
         
     }
-    
+
+   
 }
